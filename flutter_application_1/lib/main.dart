529ca6c4129213299e9081b51b2150d240972ae6
dwyudistira <yudistiradharmawardana@smkwikrama.sch.id>
2024-10-30 12:51:47 +0700

    initial commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'Cashier_page.dart'; // Import the file where CashierPage is defined
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cashier App',
+      home: const CashierPage(), // This should now work
+    );
+  }
+}
